88d1ea8ceffc7d4d224cabb708f99918d1851ad1
Regilios <ahiro2011@gmail.com>
2025-05-11 15:41:50 +0500

    update code

--- task_1_timer.js
+++ task_1_timer.js
@@ -1,1 +1,27 @@
-console.log('new task');
+'use strict';
+
+const INTERVAL = 10;
+const MAX_VALUE = 10;
+
+let counter = 0;
+let timer = null;
+
+const events = () => {
+    if (counter === MAX_VALUE) {
+        console.log('The end');
+        clearInterval(timer);
+        return;
+    }
+    console.log(
+        {
+            counter, 
+            date:new Date()
+        }
+    );
+    counter++;
+}
+
+console.log("Start");
+timer = setInterval(events, INTERVAL);
+
+
