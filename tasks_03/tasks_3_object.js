d6b1b37296ac953eb8343a9a05220eb9052aef86
Regilios <ahiro2011@gmail.com>
2025-08-14 10:55:07 +0500

    object

--- tasks_03/tasks_3_object.js
+++ tasks_03/tasks_3_object.js
@@ -1,3 +1,27 @@
+/**
+ *  Встроенные объекты.  в JS это, на самом деле, встроенные функции. Каждая из этих встроенных функций может быть использована как конструктор.
+ *  String
+    Number
+    Boolean
+    Object
+    Function
+    Array
+    Date
+    RegExp
+    Error
+ */
+var strPrimitive = "I am a string";
+typeof strPrimitive;	// "string"
+strPrimitive instanceof String;	// false
+
+var strObject = new String( "I am a string" );
+typeof strObject; // "object"
+strObject instanceof String;	// true
+// проверим подтип объекта
+Object.prototype.toString.call( strObject );	// [object String]
+
+
+
 /**Два объекта равны только в том случае, если это один и тот же объект. */
 
 let a = {};
@@ -12,3 +36,14 @@ alert( a === b ); // true
 
     alert( a == b ); // false
 }
+
+/*
+ES6 добавляет вычисляемые имена свойств, где можно указать выражение, обрамленное [ ], в качестве пары ключ-значение при литеральном объявлении объекта:
+*/
+var prefix = "foo";
+var myObject = {
+    [prefix + "bar"]: "hello",
+    [prefix + "baz"]: "world"
+};
+myObject["foobar"]; // hello
+myObject["foobaz"]; // world
